e7fb129e494bca774307fec27d75e4419972aceb
pramuditha1 <pramuditha1@live.com>
2022-03-04 08:21:12 +0530

    Adding styles to components

--- client/src/App.js
+++ client/src/App.js
@@ -5,22 +5,26 @@ import memories from './images/memories.png';
 import Posts from './components/Posts/Posts';
 import Form from './components/Form/Form';
 
+import useStyles from './styles';
+
 const App = () => {
+  const classes = useStyles();
   return (
-    <Container maxWidth='lg'>
-      <AppBar position='static' color='inherit'>
-        <Typography variant='h2' align='center'>Memories</Typography>
-        <img src={memories} alt='memories' height='60' />
+    <Container maxWidth="lg">
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">Memories</Typography>
+        <img className={classes.image} src={memories} alt="icon" height="60" />
       </AppBar>
       {/* Grow provides a simple animation */}
       <Grow in>
         <Container>
           <Grid container justify="space-between" alignItems="stretch" spacing={3}>
             <Grid item xs={12} sm={7}>
-              <Posts />
+              <Posts/>
             </Grid>
-            <Grid item xs={12} sm={4}></Grid>
-              <Form />
+            <Grid item xs={12} sm={4}>
+              <Form/>
+            </Grid>
           </Grid>
         </Container>
       </Grow>
--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import Post from './Post/Post';
+import useStyles from './styles';
 
-const Posts = () => {
+const Posts = () => {  
+  const classes = useStyles();
   return (
       <>
       <Post></Post>
       <Post></Post>
       <Post></Post>
       </>
-    
   )
 }
 
